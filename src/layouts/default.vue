652e234844dd4db70182f854d99969226e3c3a95
Stream Protocol <105049869+stream-protocol@users.noreply.github.com>
2022-06-02 13:31:10 +0300

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -41,13 +41,10 @@
 
 <script lang="ts">
 import { Vue, Component, Watch } from 'nuxt-property-decorator'
-
 import { Layout, Alert } from 'ant-design-vue'
 import { get } from 'lodash-es'
 import Setting from '@/components/Setting.vue'
-
 const { Content } = Layout
-
 @Component({
   components: {
     Layout,
@@ -59,29 +56,23 @@ const { Content } = Layout
 export default class Default extends Vue {
   pageName: string = ''
   showFlag: boolean = false
-
   @Watch('$route', { immediate: true, deep: true })
   onRouteChanged() {
     this.updateShow()
   }
-
   @Watch('$accessor.farm.infos', { immediate: true, deep: true })
   onFarmChanged() {
     this.updateShow()
   }
-
   mounted() {
     this.updateShow()
   }
-
   updateShow() {
     this.pageName = this.$route.name ?? ''
-
     for (const [poolId, farmInfo] of Object.entries(this.$accessor.farm.infos)) {
       // @ts-ignore
       if (farmInfo.version === 3 && farmInfo.legacy) {
         const userInfo = get(this.$accessor.farm.stakeAccounts, poolId)
-
         if (userInfo) {
           const { depositBalance } = userInfo
           if (!depositBalance.isNullOrZero()) {
@@ -99,7 +90,6 @@ export default class Default extends Vue {
 .ant-layout-content {
   min-height: calc(100vh - 64px - 82px);
 }
-
 // footer {
 //   width: 100%;
 //   height: 100px;
@@ -113,7 +103,6 @@ export default class Default extends Vue {
   margin-top: 30px;
   background-color: transparent;
   border: 1px solid #22252b;
-
   .anticon-close {
     color: #fff;
   }
